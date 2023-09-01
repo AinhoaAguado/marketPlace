5108fe11332ce478ff99d97d369f21cea39fea6b
catari22 <catarigeovanny@gmail.com>
2023-09-01 13:41:50 +0200

    feat(dev): creation-of-a-new-folder-with-name-functions

---
+++ src/components/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AddBtn from './functions/addBtn'
+import { useState } from 'react'
+
+
+function Home() {
+  return (
+    <>
+    
+    
+    </>
+  )
+}
+
+export default Home
