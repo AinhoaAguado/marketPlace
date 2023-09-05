2a8e55c5d09941ee04c2791ff21d986261d45e9d
catari22 <catarigeovanny@gmail.com>
2023-09-05 10:56:43 +0200

    feat(home):add-conection-with-fake-api

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import AddBtn from './functions/addBtn'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 
-
-function Home() {
+function Home() {  
+  const [Info, setInfo] = useState([])
+  useEffect( () =>{    
+    fetch("http://localhost:3000/courses").then(res => res.json())
+    .then(data => setInfo(data))
+  }, []);  
+  
   return (
-    <>
-    
-    
-    </>
+    <main>
+      <h1>Bienvenido Sergio</h1>
+      <button>Add Course</button>
+      <ul>
+      {Info.map(infoi => (
+          <li key = {infoi.id}>
+            <div>
+            {infoi.title}
+            </div>
+            <div>
+            {infoi.subtitle}
+            </div>
+            <div>
+            {infoi.description}
+            </div>
+            <div>
+            {infoi.price}
+            </div>
+            </li>
+          ))}
+      </ul>
+    </main>
   )
 }
 
-export default Home
+export default Home;
