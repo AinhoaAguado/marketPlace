57375827a23f53442a145fc48e4dba8605c7a83a
bryan <bryammd7@hotmail.com>
2023-09-06 13:00:08 +0200

    feat(components):style form

--- src/components/Organisms/AddCourseFORM/AddCourseForm.jsx
+++ src/components/Organisms/AddCourseFORM/AddCourseForm.jsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import Nav from "../navBar/Nav";
 import Footer from "../Footer/Footer";
-import Button from "../../Atoms/Button";
+import './addCourseForm.css'
 
 
 
@@ -13,15 +13,48 @@ const AddCourseForm = () => {
     <>
     <Nav />
     <form className="form" onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName")} placeholder="name" />
-      <input {...register("description")} placeholder="description" />
-      <input {...register("price")} placeholder="price" />
-      <input type="file" placeholder="img" />
-    
-      <input type="submit" />
-    </form>   
+      <div className="all">
 
-    <Link to="/"><Button text="Back" style="btn btnBlue" /></Link>
+      <h1>FORM</h1>
+
+      <label htmlFor="file__image" className="file__image" >
+        <img src="src/assets/icon-image-file.svg" alt="icon__file-upload" />
+      </label>
+      <input type="file" placeholder="img" id="file__image"/>
+
+      <div className="container__input">
+      <label htmlFor="course__title">COURSE TITLE</label>
+      <div className="container__title">
+      <input {...register("firstName")} id='course__title' placeholder="name" />
+      </div>
+      </div>
+
+
+
+      <div className="container__input">
+      <label htmlFor="course__price">PRICE</label>
+      <div className="container__price">
+      <input {...register("price")} placeholder="price" id="course__price"/>
+      </div>
+      </div>
+
+
+
+      <div className="container__input">
+      <label htmlFor="course__description">DESCRIPTION</label>
+      <div className="container__description">
+      <input {...register("description")} placeholder="description" id="course__description" />
+      </div>
+      </div>
+
+      <div className="container__buttons">
+      <input type="submit" value="ADD" id="button__submit"/>
+      <Link to="/"><button className="button__back" >BACK</button></Link>
+    </div>
+
+      </div>
+      
+    </form>   
     <Footer />
     </>
   )
